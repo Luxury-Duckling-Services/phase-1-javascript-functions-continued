68b1234c6ec5a98ac57ea3bf7538cf63fbd9258b
Yifan <datasciencewyfan@gmail.com>
2022-05-16 13:43:08 -0700

    commit tree 5679208ac8b4125664c86324c38a55027fe1b021

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(arg = 'roller-skate') {
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(act = 'go to the office') {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(visual = '*') {
+    return function (def = 'special') {
+        return `You are ${visual}${def}${visual}!`
+    }
+}
